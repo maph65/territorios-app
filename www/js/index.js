2d456773726ef83e40e9daa222a2aef0bc9ad237
Miguel Pérez <miguel.perez@brainforth.com>
2023-04-24 19:30:16 -0600

    Ajustes abril 24

--- www/js/index.js
+++ www/js/index.js
@@ -14,27 +14,18 @@ window.fn.open = function() {
 };
 
 window.fn.load = function(page) {
-    var content = document.getElementById('content');
+    console.log('Cargando ' + page);
+    //var content = document.getElementById('content');
     var menu = document.getElementById('menu');
-    content.load(page)
-        .then(menu.close.bind(menu));
-    /*if(page == 'cotizaciones.html' || page=='pedidos.html'){
-        if(page == 'cotizaciones.html'
-            && document.querySelector('#gilsaNavigator').topPage.id == 'cotizaciones-landing'){
-            console.log('cerrar Menu');
-            menu.close.bind(menu);
-        }else if(page == 'pedidos.html'
-            && document.querySelector('#gilsaNavigator').topPage.id == 'pedidos-landing'){
-            console.log('cerrar Menu');
-            menu.close.bind(menu);
-        }else{
-            document.querySelector('#gilsaNavigator')
-                .resetToPage(page).then(menu.close.bind(menu));
-        }
+    if(page !== document.querySelector('#territoriosNavigator').topPage.id + '.html'){
+        document.querySelector('#territoriosNavigator')
+            .resetToPage(page).then(menu.close.bind(menu));
     }else{
-        content.load(page)
-            .then(menu.close.bind(menu));
-    }*/
+        menu.close();
+    }
+
+    /*content.load(page)
+        .then(menu.close.bind(menu));*/
 };
 
 window.fn.popBtn = function(){
@@ -42,3 +33,41 @@ window.fn.popBtn = function(){
     document.querySelector('#territoriosNavigator')
         .popPage();
 }
+
+window.fn.goConcenos = function(){
+    document.querySelector('#territoriosNavigator')
+        .pushPage('conocenos.html',{});
+}
+
+window.fn.goSearchResults = function(){
+    document.querySelector('#territoriosNavigator')
+        .pushPage('resultado-busqueda.html',{  });
+}
+
+window.fn.goAgradecimientos = function(){
+    document.querySelector('#territoriosNavigator')
+        .pushPage('agradecimientos.html',{});
+}
+
+window.fn.goLeerMas = function(){
+    document.querySelector('#territoriosNavigator')
+        .pushPage('leer-mas.html',{});
+}
+
+window.fn.goEquipo = function(){
+    document.querySelector('#territoriosNavigator')
+        .pushPage('equipo.html',{});
+}
+window.fn.goMap = function(){
+    document.querySelector('#territoriosNavigator')
+        .popPage({times:2});
+}
+window.fn.leerMasNext = function() {
+    var carousel = document.getElementById('carousel-leer-mas');
+    carousel.next();
+};
+
+window.fn.equipoNext = function() {
+    var carousel = document.getElementById('carousel-equipo');
+    carousel.next();
+};
